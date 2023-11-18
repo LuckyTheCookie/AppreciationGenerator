20da226bfa1515ec910c385d6ad3af444e718946
Lucky <85549951+LuckyTheCookie@users.noreply.github.com>
2023-11-18 23:26:05 +0100

    🐛 Bugs fix / UI Enhanced

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Récupérer la liste des noms depuis le stockage local
     let names = JSON.parse(localStorage.getItem('names')) || [];
-    let gender;
     let schoolResults;
 
 
@@ -24,48 +23,23 @@ document.addEventListener('DOMContentLoaded', function () {
     // Appeler la fonction pour générer le dropdown au chargement de la page
     generateNameDropdown();
 
-    // Gestionnaire d'événement pour le bouton "En hausse"
-    document.getElementById('upButton').addEventListener('click', function () {
-        schoolResults = 'up';
-        console.log('School Results:', schoolResults);
-    });
+    // Gestionnaires d'événements pour chaque bouton
+    ['noWorkButton', 'badButton', 'difficultButton', 'justButton', 'absentButton', 'minimumButton', 'attitudeButton', 'encouragementButton', 'perfectButton'].forEach(function (buttonId) {
+        document.getElementById(buttonId).addEventListener('click', async function () {
+            schoolResults = buttonId.toLowerCase().replace('button', ''); // Extraire la catégorie à partir de l'ID du bouton
+            console.log('School Results:', schoolResults);
 
-    // Gestionnaire d'événement pour le bouton "Moyen / Stable"
-    document.getElementById('middleButton').addEventListener('click', function () {
-        schoolResults = 'middle';
-        console.log('School Results:', schoolResults);
-    });
+            // Générer une appréciation en fonction de la catégorie choisie
+            let appreciation = await generateAppreciation();
 
-    // Gestionnaire d'événement pour le bouton "En baisse"
-    document.getElementById('downButton').addEventListener('click', function () {
-        schoolResults = 'down';
-        console.log('School Results:', schoolResults);
-    });
-
-    // Gestionnaire d'événement pour le bouton "Homme"
-    document.getElementById('hommeButton').addEventListener('click', function () {
-        gender = 'boys';
-        console.log('Gender:', gender);
-    });
-
-    // Gestionnaire d'événement pour le bouton "Femme"
-    document.getElementById('femmeButton').addEventListener('click', function () {
-        gender = 'girls';
-        console.log('Gender:', gender);
-    });
-
-    // Gestionnaire d'événement pour le bouton "Générer"
-    document.getElementById('generateButton').addEventListener('click', async function () {
-        // Générer une appréciation en fonction de la catégorie choisie
-        let appreciation = await generateAppreciation();
-
-        // Afficher l'appréciation sous le dropdown
-        displayAppreciation(appreciation);
+            // Afficher l'appréciation sous le dropdown
+            displayAppreciation(appreciation);
+        });
     });
 
     // Fonction pour générer une appréciation en fonction de la catégorie choisie
     async function generateAppreciation() {
-        let categoryFile = `${schoolResults}-${gender}.txt`; // Nom du fichier en fonction de la catégorie et du genre choisis
+        let categoryFile = `./Appreciations/${schoolResults}.txt`; // Nom du fichier en fonction de la catégorie choisie
         
         try {
             // Utiliser Fetch API pour charger le contenu du fichier
@@ -81,11 +55,14 @@ document.addEventListener('DOMContentLoaded', function () {
             // Récupérer le nom sélectionné dans le dropdown
             let selectedName = document.getElementById('nameDropdown').value;
 
-            // Déterminer si le nom commence par une voyelle
-            let startsWithVowel = /^[aeiouAEIOU]/.test(selectedName);
+            // Déterminer si le nom commence par une voyelle ou un H
+            let startsWithVowel = /^[aeiouhAEIOUH]/.test(selectedName);
 
             // Remplacer "(determinant)" par "d'" ou "de " en fonction de la voyelle
             let determinantReplacement = startsWithVowel ? "d'" : "de ";
+
+            // Remplacer "X" par le nom sélectionné dans chaque ligne
+            data = data.map(line => line.replace(/X/g, selectedName));
 
             // Sélectionner une appréciation au hasard parmi les appréciations disponibles
             let appreciation = '';
@@ -97,7 +74,7 @@ document.addEventListener('DOMContentLoaded', function () {
             return appreciation.trim();
         } catch (error) {
             console.error(error.message);
-            return 'Erreur lors de la génération de l\'appréciation'; // Retourner un message d'erreur en cas d'échec
+            return 'Je suis désolé, mais une erreur est survenue. C\'est assez étrange... Redémarrez la page puis réessayez :)'; // Retourner un message d'erreur en cas d'échec
         }
     }
 
